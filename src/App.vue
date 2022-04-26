ec400264f80d9c1921377721dfad714bddb82095
huangyz <huangyz@qzccbank.com>
2022-04-26 17:19:03 +0800

    引入Element Plus

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark"/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"/>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light"/>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  .transition-box {
+    margin-bottom: 10px;
+    width: 200px;
+    height: 100px;
+    border-radius: 4px;
+    background-color: #409eff;
+    text-align: center;
+    color: #fff;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    margin-right: 20px;
+  }
+
+  .el-row {
+    margin-bottom: 20px;
+  }
+
+  .el-row:last-child {
+    margin-bottom: 0;
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+  .bg-purple-dark {
+    background-color: purple;
+  }
+
+  .bg-purple {
+    background-color: blue;
+  }
+
+  .bg-purple-light {
+    background-color: lightblue;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+// collapse
+import {ElCollapseTransition} from 'element-plus'
+// fade/zoom
+// import 'element-plus/lib/theme-chalk/base.css'
+
+const app = createApp(App)
+app.component(ElCollapseTransition.name, ElCollapseTransition)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
